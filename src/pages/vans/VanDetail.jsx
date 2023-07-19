beb81eaa6a964ab3b8ce8ca3097872c2473a80c9
ihnaqi <ih.naqi@outlook.com>
2023-07-19 11:14:19 +0500

    fixed getVan API

--- src/pages/vans/VanDetail.jsx
+++ src/pages/vans/VanDetail.jsx
@@ -1,8 +1,8 @@
 import { Link, useLocation, useLoaderData } from "react-router-dom"
-import { getVans } from "../../api/api"
+import { getVan } from "../../api/api"
 
 export function loader({params}) {
-    return getVans(params.id)
+    return getVan(params.id)
 }
 
 export default function VanDetail () {
